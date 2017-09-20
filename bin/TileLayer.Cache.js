36f1ee3a69ab7ef003fe1b1bdf2d107f95edbe04
Ryan Constantino <ryan.constantino93@gmail.com>
2017-09-19 21:57:50 -0700

    Set up tsconfig.

---
+++ bin/TileLayer.Cache.js
@@ -0,0 +1,13 @@
+System.register("TileLayer.Cache", [], function (exports_1, context_1) {
+    "use strict";
+    var __moduleName = context_1 && context_1.id;
+    function TileLayerCache() {
+        console.warn(`TileLayerCache() called`);
+    }
+    exports_1("TileLayerCache", TileLayerCache);
+    return {
+        setters: [],
+        execute: function () {
+        }
+    };
+});
